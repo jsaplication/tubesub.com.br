24147a3104136b763e19708e44e30bd416fad2f9
JSaplication <67569252+jsaplication@users.noreply.github.com>
2025-07-10 16:07:08 -0700

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -176,14 +176,31 @@ function getCounts(limite){
 
 			options  = `<div class="btn-remove" uid="${v.uid}" onclick="select(event)"></div>
 				        	<div class="select select_${v.uid}">
-				        		<div class="option" onclick="deleteSession(this)" uid="${v.uid}"><div class="icon-option" style="background-image: url(icones/delete.png);"></div>Excluir Sessão</div>
-				        		<div class="option" onclick="edit(event)" uid="${v.uid}"><div class="icon-option" style="background-image: url(icones/edit.png);"></div>Renomar Sessão</div>
+				        		<div class="option" onclick="deleteSession(this)" uid="${v.uid}">
+					        		<div class="icon-option">
+										<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+										  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+										</svg>
+
+					        		</div>
+
+					        		Excluir Sessão
+				        		</div>
+				        		<div class="option" onclick="edit(event)" uid="${v.uid}">
+					        		<div class="icon-option">
+											<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+											  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
+											</svg>
+					        		</div>
+
+					        		Renomar Sessão
+				        		</div>
 				        		<!--<div class="option" onclick="editCapa(this)" uid="${v.uid}"><div class="icon-option" style="background-image: url(icones/capa.png);"></div>Alterar Foto da Sessão</div>-->
 				        	</div>`;
 		
 
-			if(v.type === 'whatsapp'){
-				iconetype = './icones/whatsapp2.png';
+			if(v.type === 'youtube'){
+				iconetype = './icones/youtube.webp';
 			}else{
 				iconetype = '';
 			}
